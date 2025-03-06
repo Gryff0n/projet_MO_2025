d4851b35a5d3957426b3c22dcd98f6b46d0d5f67
Kyllian <kyllian.buffaut@etu.univ-orleans.fr>
2025-03-06 11:07:27 +0100

    ajout package

---
+++ controleur/Controleur.java
@@ -0,0 +1,13 @@
+package controleur;
+
+import modele.Joueur;
+import vue.Ihm;
+
+public class Controleur {
+    Ihm ihm;
+    Joueur joueur1;
+    Joueur joueur2;
+    public Controleur(Ihm ihm) {
+        this.ihm = ihm;
+    }
+}
