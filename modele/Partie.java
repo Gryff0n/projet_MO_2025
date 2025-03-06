5a4c3dd9551e0da62a6edbe042d70fe445a60be1
Kyllian <kyllian.buffaut@etu.univ-orleans.fr>
2025-03-06 12:57:22 +0100

    petite modif du toString du plateau

--- modele/Partie.java
+++ modele/Partie.java
@@ -14,18 +14,21 @@ public class Partie {
     public boolean coupValide(int l, int c, Joueur j){
         if (l < 0 || l > 7 || c < 0 || c > 7) {
             return false;
-        } else if (tableau[l][c] != 0) {
-            return false;
-        } else {
-            return true;
-        }
+        } else return tableau[l][c] == 0;
     }
 
     @Override
     public String toString() {
-        String s="";
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
+        String s="   ";
+        int nbColonne = tableau[0].length;
+        int nbLigne = tableau.length;
+        for (int i = 0; i < nbColonne; i++){
+            s+= " "+(char) ('A' + i)+" " ;
+        }
+        s+="\n";
+        for (int i = 0; i < nbLigne; i++) {
+            s+=" "+i+" ";
+            for (int j = 0; j < nbColonne; j++) {
                 if (tableau[i][j] == 0) {
                     s+=" \uD83D\uDFE9 ";
                 } else if (tableau[i][j] == 1) {
